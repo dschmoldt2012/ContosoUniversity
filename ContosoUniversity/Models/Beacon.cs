69e952a0a1b67f90b2fbe06615649af433145940
Daryl Schmoldt <daryl.schmoldt@gmail.com>
2015-05-17 20:09:32 -0400

    Added New Models. Installed FontAwesome. Updated Bootstrap.

---
+++ ContosoUniversity/Models/Beacon.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Models
+{
+    class Beacon
+    {
+        public int BeaconID { get; set; }
+    }
+}
